2646aeb3c70ac5b2b64f4bb5aea29c9afb5fbfe1
DrewNov <DrewNov@users.noreply.github.com>
2018-06-12 14:10:50 +0300

    refactored and small code improvements

--- main.py
+++ main.py
@@ -1,72 +1,86 @@
-import sys,pygame,time,math
+import sys,pygame,math
+
+resolution = (800,600)
+a,b,c = 100,400,300
+cam_z_init = 500
+k = 0.00125
+phi = 1/600
+cp = math.cos(phi)
+sp = math.sin(phi)
+
+BLACK=(0,0,0)
+WHITE=(255,255,255)
+RED=(255,0,0)
+GREEN=(0,255,0)
+BLUE=(0,0,255)
+
+cube1 = [[-a,-a,-a],[a,-a,-a],[a,a,-a],[-a,a,-a],[-a,-a,a],[a,-a,a],[a,a,a],[-a,a,a]]
+cube2 = [[0,0,b],[a,0,b],[a,a,b],[0,a,b],[0,0,a+b],[a,0,a+b],[a,a,b+a],[0,a,a+b]]
 
 pygame.init()
-
-screen = pygame.display.set_mode((800,600))
-
-c = 200
-
-cube1 = [[-c/2,-c/2,-c/2],[c/2,-c/2,-c/2],[c/2,c/2,-c/2],[-c/2,c/2,-c/2],[-c/2,-c/2,+c/2],[c/2,-c/2,+c/2],[c/2,c/2,+c/2],[-c/2,c/2,+c/2]]
-cube2 = [[0,0,400],[c/2,0,400],[c/2,c/2,400],[0,c/2,400],[0,0,400+c/2],[c/2,0,400+c/2],[c/2,c/2,400+c/2],[0,c/2,400+c/2]]
-
-cam_z_1 = 500
-
-k = 0.00125
-
-phi = 1/600
+screen = pygame.display.set_mode(resolution)
 
 
 def draw_cube(cube,cam_z):
-        for i in range(0,8):
+        for i in range(8):
                 if i == 3 or i == 7:
-                        pygame.draw.line(screen,(255,0,0),(300 + cube[i][0]/(k*(cube[i][2]+cam_z)),300 + cube[i][1]/(k*(cube[i][2]+cam_z))),(300 + cube[i-3][0]/(k*(cube[i-3][2]+cam_z)),300 + cube[i-3][1]/(k*(cube[i-3][2]+cam_z))))
-
+                        pygame.draw.line(screen, RED,
+                            (c + cube[i][0]/(k*(cube[i][2]+cam_z)), c + cube[i][1]/(k*(cube[i][2]+cam_z))),
+                            (c + cube[i-3][0]/(k*(cube[i-3][2]+cam_z)), c + cube[i-3][1]/(k*(cube[i-3][2]+cam_z))))
                 else:
-                        pygame.draw.line(screen,(0,255,0),(300 + cube[i][0]/(k*(cube[i][2]+cam_z)),300 + cube[i][1]/(k*(cube[i][2]+cam_z))),(300 + cube[i+1][0]/(k*(cube[i+1][2]+cam_z)),300 + cube[i+1][1]/(k*(cube[i+1][2]+cam_z))))
+                        pygame.draw.line(screen, GREEN,
+                            (c + cube[i][0]/(k*(cube[i][2]+cam_z)), c + cube[i][1]/(k*(cube[i][2]+cam_z))),
+                            (c + cube[i+1][0]/(k*(cube[i+1][2]+cam_z)), c + cube[i+1][1]/(k*(cube[i+1][2]+cam_z))))
 
                         if i <= 3:
-                                pygame.draw.line(screen,(0,0,255),(300 + cube[i][0]/(k*(cube[i][2]+cam_z)),300 + cube[i][1]/(k*(cube[i][2]+cam_z))),(300 + cube[i+4][0]/(k*(cube[i+4][2]+cam_z)),300 + cube[i+4][1]/(k*(cube[i+4][2]+cam_z))))
+                                pygame.draw.line(screen, BLUE,
+                                    (c + cube[i][0]/(k*(cube[i][2]+cam_z)), c + cube[i][1]/(k*(cube[i][2]+cam_z))),
+                                    (c + cube[i+4][0]/(k*(cube[i+4][2]+cam_z)), c + cube[i+4][1]/(k*(cube[i+4][2]+cam_z))))
 
-                #Because of 1st if statement we need to do additional drawing
-                pygame.draw.line(screen,(255,255,255),(300 + cube[3][0]/(k*(cube[3][2]+cam_z)),300 + cube[3][1]/(k*(cube[3][2]+cam_z))),(300 + cube[7][0]/(k*(cube[7][2]+cam_z)),300 + cube[7][1]/(k*(cube[7][2]+cam_z))))
+        #Because of 1st 'if statement' we need to do additional drawing
+        pygame.draw.line(screen, WHITE,
+            (c + cube[3][0]/(k*(cube[3][2]+cam_z)), c + cube[3][1]/(k*(cube[3][2]+cam_z))),
+            (c + cube[7][0]/(k*(cube[7][2]+cam_z)), c + cube[7][1]/(k*(cube[7][2]+cam_z))))
 
         pygame.display.update()
 
 
 def cube_coord_change(cube,cam_z):
-        pressed = pygame.key.get_pressed()            
-        if pressed[pygame.K_d]:
-                for i in cube:
-                        i[0] = i[0]*math.cos(phi)+i[2]*math.sin(phi)
-                        i[2] = i[2]*math.cos(phi)-i[0]*math.sin(phi)
-        elif pressed[pygame.K_a]:
-                for i in cube:
-                        i[0] = i[0]*math.cos(-phi)+i[2]*math.sin(-phi)
-                        i[2] = i[2]*math.cos(-phi)-i[0]*math.sin(-phi)
+        pressed = pygame.key.get_pressed()   
+
+        if pressed[pygame.K_a]:
+                for v in cube:
+                        v[0] = v[0]*cp + v[2]*sp
+                        v[2] = v[2]*cp - v[0]*sp
+        elif pressed[pygame.K_d]:
+                for v in cube:
+                        v[0] = v[0]*cp - v[2]*sp
+                        v[2] = v[2]*cp + v[0]*sp
         elif pressed[pygame.K_w]:
-                for i in cube:
-                        i[1] = i[1]*math.cos(phi)+i[2]*math.sin(phi)
-                        i[2] = i[2]*math.cos(phi)-i[1]*math.sin(phi)
+                for v in cube:
+                        v[1] = v[1]*cp + v[2]*sp
+                        v[2] = v[2]*cp - v[1]*sp
         elif pressed[pygame.K_s]:
-                for i in cube:
-                        i[1] = i[1]*math.cos(-phi)+i[2]*math.sin(-phi)
-                        i[2] = i[2]*math.cos(-phi)-i[1]*math.sin(-phi)
+                for v in cube:
+                        v[1] = v[1]*cp - v[2]*sp
+                        v[2] = v[2]*cp + v[1]*sp
+
         elif pressed[pygame.K_LEFT]:
-                for i in cube:
-                        i[0] += 1
+                for v in cube:
+                        v[0] -= 1
         elif pressed[pygame.K_RIGHT]:
-                for i in cube:
-                        i[0] -= 1
+                for v in cube:
+                        v[0] += 1
+
         elif pressed[pygame.K_UP]:
-                for i in cube:
-                        cam_z -= 0.1
+                        cam_z -= 1
         elif pressed[pygame.K_DOWN]:
-                for i in cube:
-                        cam_z += 0.1 
+                        cam_z += 1
 
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                        pass
+                        pygame.quit()
+                        sys.exit()
         
         return cam_z
 
@@ -74,11 +88,8 @@ def cube_coord_change(cube,cam_z):
 def move_cube(cube,cam_z):
         while True:
                 draw_cube(cube,cam_z)
-                
                 cam_z = cube_coord_change(cube,cam_z)
+                screen.fill(BLACK)
 
-                screen.fill((0,0,0)) 
 
-
-move_cube(cube1,cam_z_1)
-
+move_cube(cube1,cam_z_init)
